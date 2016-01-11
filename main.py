cacdc27ebfdd0d14a269597650414c3f83fce738
L. Chin <ltchin@mit.edu>
2016-01-11 00:00:37 -0500

    Tried out speed PID - breaks b/c of the update things Eric was talking about

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import time
 if __name__ == '__main__':
 	with tamproxy.TAMProxy() as proxy:
 		robot = hal.Robot(proxy)
-		robot.drive.setDistSetpoint(3200)
+		# robot.drive.setDistSetpoint(3200)
+		robot.drive.go(0.1, 0)
 		currentTime = time.time()
 		while(True):    
 			if time.time() - currentTime > 0.01:
 				currentTime = time.time()
-				robot.drive.distPIDIterate()
+				robot.drive.speedPIDIterate()
+				# robot.drive.distPIDIterate()
