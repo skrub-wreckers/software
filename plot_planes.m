c8811508c526ed2de302231f28e9c0b94e3ea626
Eric Wieser <wieser.eric@gmail.com>
2016-01-11 11:30:51 -0500

    Colorize planes by the thing that they threshold, use the same planes as cvtest

--- plot_planes.m
+++ plot_planes.m
@@ -2,8 +2,8 @@ load('data-trunc.mat')
 figure()
 hold on
 axis equal
-%scatter3(green(:,3), green(:,2), green(:,1), 'g')
-%scatter3(red(:,3), red(:,2), red(:,1),'r')
+scatter3(green(:,3), green(:,2), green(:,1), 'g')
+scatter3(red(:,3), red(:,2), red(:,1),'r')
 scatter3(blue(:,3), blue(:,2), blue(:,1), 'b')
 xlim([0 255])
 ylim([0 255])
@@ -23,21 +23,21 @@ b = [0 0; 255 255];
 
 
 dirs = [
-	[-1 1.3 0];
-	[-1 0 1.3];
-	[-1 0.65 0.65];
-    [-0.5 -0.65 0.65];
+	{[-1 0.65 0.65]', 'r'};
+	{[-1.3 1 0]', 'g'};
+	{[0.01 1 -1.3]', 'g'};
+    {[-0.5 -0.65 0.65]', 'b'};
 ]';
 for dir=dirs
+	[dir, col] = dir{:}
 	r1 = -(dir(2)*g + dir(3)*b) / dir(1);
-	surf(r1, g, b)
-	alpha(.4)
+	s = surf(r1, g, b, 'FaceColor', col, 'FaceAlpha', 0.4);
 	center = [128 128 128]';
 	c_plane = center - dir*(dir'*center);
 	
 	q = quiver3(c_plane(1), c_plane(2), c_plane(3),...
 		    64*dir(1), 64*dir(2), 64*dir(3),...
-			'k');
+			col);
 	q.LineWidth = 2;
 	q.MaxHeadSize = 8;
 	
