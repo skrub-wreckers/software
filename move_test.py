30043edb20135511d0937669465aeaa6cd7ff4e9
Eric Wieser <wieser.eric@gmail.com>
2016-01-15 14:01:11 -0500

    Trailing spaces

--- move_test.py
+++ move_test.py
@@ -11,11 +11,11 @@ if __name__ == "__main__":
 		#arm = Arm(tamproxy, 10)
 		#arm2 = Arm(tamproxy, 9)
 		arms = Arms(tamproxy)
-		
+
 		w = Window("Eric")
 
 		while True:
-			
+
 			c = cv2.waitKey(1) & 0xFF
 			if c == ord('q'):
 				break
@@ -23,27 +23,27 @@ if __name__ == "__main__":
 				drive.go(0.2)
 				time.sleep(0.25)
 				drive.go(0.0)
-				
+
 			elif c == ord('s'):
 				drive.go(-0.2)
 				time.sleep(0.25)
 				drive.go(0.0)
-				
+
 			elif c == ord('a'):
 				drive.turnIP(0.2)
 				time.sleep(0.25)
 				drive.turnIP(0.0)
-				
+
 			elif c == ord('d'):
 				drive.turnIP(-0.2)
 				time.sleep(0.25)
 				drive.turnIP(0.0)
-				
+
 			elif c == ord(' '):
 				arms.green.up()
 				time.sleep(0.75)
 				arms.green.down()
-				
+
 			elif c == ord('c'):
 				arms.red.up()
 				time.sleep(0.75)
