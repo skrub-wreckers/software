18f4e64134642820a65de945b30967999132669e
Eric Wieser <wieser.eric@gmail.com>
2016-01-18 23:25:33 -0500

    Deal with far off cubes, spin faster

--- vision_move_test.py
+++ vision_move_test.py
@@ -33,21 +33,24 @@ if __name__ == "__main__":
 
             if cube is None:
                 print "No cube"
-                drive.go(0, 0.05)
+                drive.go(0, 0.1)
 
-            elif abs(cube.angle_to) < np.radians(5):
+            elif abs(cube.angle_to) < np.radians(3):
                 print "Going {}in to {}".format(cube.distance, cube)
-                # todo: steer while moving?
-                drive.go_distance(cube.distance + 1)
+                if cube.distance > 60:
+                    drive.go_distance(60)
+                else:
+                    # todo: steer while moving?
+                    drive.go_distance(cube.distance + 1)
 
-                if cube.color == Colors.GREEN:
-                    arms.green.up()
-                    time.sleep(0.75)
-                    arms.green.down()
-                elif cube.color == Colors.RED:
-                    arms.red.up()
-                    time.sleep(0.75)
-                    arms.red.down()
+                    if cube.color == Colors.GREEN:
+                        arms.green.up()
+                        time.sleep(0.75)
+                        arms.green.down()
+                    elif cube.color == Colors.RED:
+                        arms.red.up()
+                        time.sleep(0.75)
+                        arms.red.down()
             else:
                 print "Turning {} to {}".format(cube.angle_to, cube)
                 drive.turn_angle(cube.angle_to)
