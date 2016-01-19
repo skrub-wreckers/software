ebd0546e50e0819d9efb44abe9d01b5aef831af2
Andrew Reilley <areill1337@gmail.com>
2016-01-19 02:11:37 -0500

    Added odometer test script

---
+++ odometer_test.py
@@ -0,0 +1,19 @@
+import pins
+from tamproxy.devices import Odometer
+from tamproxy.devices import Gyro
+from tamproxy.devices import Encoder
+from tamproxy import TAMProxy
+
+import time
+
+if __name__ == "__main__":
+    tamp = TAMProxy()
+    gyro = Gyro(tamp, pins.gyro_cs, integrate=False)
+    lEnc = Encoder(tamp, pins.l_encoder_a, pins.l_encoder_b, continuous = False)
+    rEnc = Encoder(tamp, pins.r_encoder_a, pins.r_encoder_b, continuous = False)
+    odo = Odometer(tamp, lEnc, rEnc, gyro, 0.05)
+    
+    while True:
+        odo.update()
+        print odo.val
+        time.sleep(0.05)
