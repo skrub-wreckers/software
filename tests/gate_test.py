1dc2b768c579cc47ea24d02b36e794fd2d03a78c
Eric Wieser <wieser.eric@gmail.com>
2016-01-22 18:05:34 -0500

    Add breakbeam/gate test

---
+++ tests/gate_test.py
@@ -0,0 +1,24 @@
+from tamproxy.devices import *
+from tamproxy import TAMProxy
+import time
+from sw.gui import Window
+
+if __name__ == "__main__":
+    with TAMProxy() as tamp:
+        led = DigitalOutput(tamp, 0)
+        reading = DigitalInput(tamp, 15)
+        w = Window(500, [])
+
+        led_on = False
+
+        while True:
+            print led_on, reading.val
+            time.sleep(0.1)
+
+            try:
+                c = w.keys.get_nowait()
+                if c == ' ':
+                    led_on = not led_on
+                    led.write(led_on)
+            except:
+                pass
