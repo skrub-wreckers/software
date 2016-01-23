7f7d0cdf23229ad0e08e3f9cef0b0e9604fdd6a9
Eric Wieser <wieser.eric@gmail.com>
2016-01-22 22:15:06 -0500

    Add comments, improve test

--- sw/taskqueue.py
+++ sw/taskqueue.py
@@ -4,46 +4,41 @@ import inspect
 
 class TaskQueue(object):
     def __init__(self):
-        # contains the current operation. Setting this is locked through
-        # _busy_lock. Upon completion, this is cleared locked on op_done.
-        self._op = None
-        self._busy_lock = threading.Condition()
-        self._op_done = threading.Condition()
+        # contains the current task. Setting this is locked through _task_lock.
+        self._task = None
+        self._task_lock = threading.Condition()
 
         self._thread = threading.Thread(target=self._background)
         self._thread.daemon = True
         self._thread.start()
 
     def _background(self):
-        """ run in the background """
-        while True:
-            # lock on _busy_lock to prevent the main thread from changing the
-            # command without it being looked at
-            with self._busy_lock:
+        with self._task_lock:
+            while True:
                 # get the next operation when notified by the main thread
-                while self._op is None:
-                    self._busy_lock.wait()
-                op = self._op
-
-                # delegate to the appropriate control method
-                op._run()
-
-                # notify the main thread that we finished the command
-                with self._op_done:
-                    self._op_done.notify_all()
-                    self._op = None
+                while self._task is None:
+                    self._task_lock.wait()
+                
+                # run the task
+                self._task._run()
+                self._task = None
 
     def enqueue(self, task):
+        """
+        Enqueue a function to run, and block until it starts. Returns a Task
+        object
+        """
         task = Task(task)
 
         # wait until the background thread is ready
-        with self._busy_lock:
-            self._busy_lock.notify()
-            self._op = task
+        with self._task_lock:
+            self._task_lock.notify()
+            self._task = task
 
         return task
 
 class TaskCancelled(Exception):
+    """Thrown at any `yield` in a task, if a cancel is requested"""
     pass
 
 class Task(object):
@@ -56,6 +51,12 @@ class Task(object):
         self._cancel_pending = threading.Event()
 
     def wait(self, *args, **kwargs):
+        """
+        Wait for the task to complete. Takes an optional timeout argument, and
+        returns true if the task completed.
+
+        If the task threw an exception on exit, rethrow it
+        """
         res = self._done.wait(*args, **kwargs)
 
         if res and self._exc_info is not None:
@@ -64,10 +65,20 @@ class Task(object):
         return res
 
     def cancel(self):
+        """
+        Request a cancel of the task
+
+        Return true if the cancel was acknowledged, and false if the task was
+        about to exit anyway.
+        """
         self._cancel_pending.set()
         self.wait()
+        return self._cancelled
 
     def _run(self):
+        """
+        Private function called by the TaskQueue
+        """
         try:
             # call the function
             gen = self.f()
@@ -92,5 +103,5 @@ class Task(object):
             self._cancelled = True
         except Exception:
             self._exc_info = sys.exc_info()
-
+        
         self._done.set()
--- task_test.py
+++ task_test.py
@@ -2,22 +2,35 @@ from sw.taskqueue import TaskQueue, TaskCancelled
 import time
 
 def print_ints():
-    i = 0
-    while True:
-        print i
-        i = i + 1
-        time.sleep(0.25)
-        try:
+    try:
+        i = 0
+        while True:
+            print i
+            i = i + 1
+            time.sleep(0.25)
             yield
-        except TaskCancelled:
-            print('No more printing')
-            raise
+    except TaskCancelled:
+        print('No more printing')
+        raise
 
+def print_once():
+    print "once"
+
+
+def throw_once():
+    raise "once"
 
 queue = TaskQueue()
 
+t = queue.enqueue(print_once)
+t.wait()
+
+t = queue.enqueue(throw_once)
+try:
+    t.wait()
+except Exception as e:
+    print(type(e))
+
 t = queue.enqueue(print_ints)
-
 time.sleep(1)
-
 t.cancel()
