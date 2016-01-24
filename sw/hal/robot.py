080ea918c0ee3cc5608d79939f2fb62ec1ca988f
Eric Wieser <wieser.eric@gmail.com>
2016-01-24 16:58:30 -0500

    Add ir sensors to the robot object

--- sw/hal/robot.py
+++ sw/hal/robot.py
@@ -1,4 +1,7 @@
 from . import HardwareDevice, Arms, Drive, RegulatedDrive, ColorSensor
+from tamproxy.devices import LongIR, ShortIR
+from .. import pins
+
 
 class Robot(HardwareDevice):
     def __init__(self, tamp):
@@ -6,3 +9,10 @@ class Robot(HardwareDevice):
         self.arms = Arms(self.tamp)
         self.drive = Drive(self.tamp)
         self.color_sensor = ColorSensor(self.tamp)
+
+        self.left_long_ir = LongIR(self.tamp, pins.l_ir_long)
+        self.right_long_ir = LongIR(self.tamp, pins.r_ir_long)
+
+        self.left_short_ir = ShortIR(self.tamp, pins.l_ir_short)
+        self.right_short_ir = ShortIR(self.tamp, pins.r_ir_short)
+        self.back_short_ir = ShortIR(self.tamp, pins.b_ir_short)
