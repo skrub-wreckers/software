a6e9ad3c8bb018c7f9cf9ed0e125c792e2eb2f4d
Eric Wieser <wieser.eric@gmail.com>
2016-01-25 16:17:02 -0500

    Expose breakbeams from hal

--- sw/hal/__init__.py
+++ sw/hal/__init__.py
@@ -4,5 +4,5 @@ from .arms import Arms
 from .odometer import Odometer
 from .drive import Drive
 from .regulateddrive import RegulatedDrive
-from .sensors import ColorSensor, DigitalIR
+from .sensors import ColorSensor, DigitalIR, BreakBeams
 from .robot import Robot
