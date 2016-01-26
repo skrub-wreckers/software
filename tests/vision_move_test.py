af77fe5f59a447b5abcf16fc474249af199a4c13
Eric Wieser <wieser.eric@gmail.com>
2016-01-25 19:28:47 -0500

    Add timeout

--- tests/vision_move_test.py
+++ tests/vision_move_test.py
@@ -10,11 +10,80 @@ import sw.constants as constants
 from sw.mapping import Mapper
 import time
 
+from sw.taskqueue import TaskCancelled
+
 
 OUR_COLOR = Colors.RED
 
 
 THEIR_COLOR = (Colors.RED | Colors.GREEN) & ~OUR_COLOR
+
+
+ROUND_TIME = 20
+SILO_TIME = ROUND_TIME - 30
+
+def pick_up_cubes(r):
+    while True:
+        val = r.color_sensor.val
+        if val == OUR_COLOR:
+            r.drive.stop()
+            r.arms.silo.up()
+            time.sleep(1.0)
+            r.arms.silo.down()
+        elif val == THEIR_COLOR:
+            r.drive.stop()
+            r.arms.dump.up()
+            time.sleep(0.75)
+            r.arms.dump.down()
+        else:
+            break
+        yield
+
+def main(r):
+    while True:
+        # pick up any cubes we have
+        for _ in pick_up_cubes(r):
+            yield
+
+        try:
+            v.update()
+        except IOError:
+            continue
+        m.setCubePositions(v.cubes)
+        cube = v.nearest_cube()
+        #print cube
+
+        if cube is None:
+            print "No cube"
+            r.drive.go(0, 0.1)
+        elif abs(cube.angle_to) < np.radians(10):
+            print "Going {}in to {}".format(cube.distance, cube)
+            # limit distance
+            to_go = cube.pos2
+            if cube.distance > 60:
+                to_go = to_go * 60 / cube.distance
+
+            # transform to world space
+            to_go = np.append(to_go, 1)
+            dest = r.drive.odometer.robot_matrix.dot(to_go)
+
+            task = r.drive.go_to(dest[:2], async=True)
+            while not task.wait(0):
+                if r.break_beams.blocked or r.color_sensor.val != Colors.NONE:
+                    task.cancel()
+                try:
+                    yield
+                except TaskCancelled:
+                    task.cancel()
+                    raise
+
+            for _ in pick_up_cubes(r):
+                yield
+
+        else:
+            print "Turning {} to {}".format(cube.angle_to, cube)
+            r.drive.turn_angle(cube.angle_to)
+
 
 if __name__ == "__main__":
     with TAMProxy() as tamproxy:
@@ -25,53 +94,24 @@ if __name__ == "__main__":
         v = Vision(cam)
         w = Window(500, [m, CameraPanel(500, v)])
 
-        def pick_up_cubes():
-            while True:
-                val = r.color_sensor.val
-                if val == OUR_COLOR:
-                    r.drive.stop()
-                    r.arms.silo.up()
-                    time.sleep(1.0)
-                    r.arms.silo.down()
-                elif val == THEIR_COLOR:
-                    r.drive.stop()
-                    r.arms.dump.up()
-                    time.sleep(0.75)
-                    r.arms.dump.down()
-                else:
-                    break
+        while w.get_key() != ' ':
+            pass
 
-        while True:
-            # pick up any cubes we have
-            pick_up_cubes()
+        start_time = time.time()
 
-            try:
-                v.update()
-            except IOError:
-                continue
-            m.setCubePositions(v.cubes)
-            cube = v.nearest_cube()
-            #print cube
+        time_up = lambda: (time.time() - start_time) >= SILO_TIME
 
-            if cube is None:
-                print "No cube"
-                r.drive.go(0, 0.1)
-            elif abs(cube.angle_to) < np.radians(10):
-                print "Going {}in to {}".format(cube.distance, cube)
-                # limit distance
-                to_go = cube.pos2
-                if cube.distance > 60:
-                    to_go = to_go * 60 / cube.distance
+        task = main()
 
-                # transform to world space
-                to_go = np.append(to_go, 1)
-                dest = r.drive.odometer.robot_matrix.dot(to_go)
 
-                task = r.drive.go_to(dest[:2], async=True)
-                while not task.wait(0):
-                    if r.break_beams.blocked or r.color_sensor.val != Colors.NONE:
-                        task.cancel()
-                pick_up_cubes()
-            else:
-                print "Turning {} to {}".format(cube.angle_to, cube)
-                r.drive.turn_angle(cube.angle_to)
+        while not time_up():
+            task.next()
+
+        try:
+            task.throw(TaskCancelled())
+        except TaskCancelled, StopIteration:
+            pass
+
+        r.arms.silo_door.open()
+        time.sleep(0.5)
+        Drive.go_distance(r.drive, 4)
