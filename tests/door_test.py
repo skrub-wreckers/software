ae8ad85303985f63271c680c3b459d1fa3339810
L. Chin <ltchin@mit.edu>
2016-01-26 22:02:13 -0500

    Try going continuously

--- tests/door_test.py
+++ tests/door_test.py
@@ -10,7 +10,6 @@ import cv2
 
 """ Make sure the robot has enough clearance to release blocks """
 
-N = 20
 CLOSE_TO_WALL = 13
 
 if __name__ == '__main__':
@@ -22,13 +21,9 @@ if __name__ == '__main__':
         # If went all the way around without finding anything, pick the best direction 
         # = combine with side sensors and knowing angles
         
-        # go every 10 degrees and check out what we've got
-        # Turn around until the front of the robot is clear for N inches
-        for i in xrange(N):
-            print r.left_long_ir.distInches, r.right_long_ir.distInches
-            if r.left_long_ir.distInches >= CLOSE_TO_WALL and r.right_long_ir.distInches >= CLOSE_TO_WALL:
-                break
-            r.drive.turn_angle(2*pi / N)
+        # Turn around until the front of the robot is clear for N inches or it has gone 360
+        while r.left_long_ir.distInches >= CLOSE_TO_WALL and r.right_long_ir.distInches >= CLOSE_TO_WALL:
+            r.drive.go(0, 0.1)
 
         r.arms.silo_door.write(180)
         time.sleep(0.5)
