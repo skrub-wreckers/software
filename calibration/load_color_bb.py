0d4797a411319b0d3248fef0daab93a286fcdb8d
Eric Wieser <wieser.eric@gmail.com>
2016-01-28 17:39:20 -0500

    Plot projected values

--- calibration/load_color_bb.py
+++ calibration/load_color_bb.py
@@ -1,7 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-fix, axs = plt.subplots(3, 2)
+from sw.hal import ColorSensor
+
+fix, axs = plt.subplots(3, 3)
+# fig2, axs2 = plt.subplots(3, 2)
+
+bbs = {}
+colors = {}
 
 for i, cname in enumerate(['none', 'red', 'green']):
     try:
@@ -10,17 +16,32 @@ for i, cname in enumerate(['none', 'red', 'green']):
     except:
         continue
 
-    ax1, ax2 = axs[i,:]
+    bbs[cname] = bb
+    colors[cname] = color
+
+    ax1, ax2, ax3 = axs[i,:]
 
     ax1.plot(bb[:,0], label='Left')
     ax1.plot(bb[:,1], label='Right')
-    ax1.set(ylim=[0, 65536], title='Breakbeams')
+    ax1.legend()
+    ax1.set(ylim=[0, 32768], title='Breakbeams ({})'.format(cname))
+    ax1.grid()
 
 
     ax2.plot(color[:,0], color='r', label='R')
     ax2.plot(color[:,1], color='g', label='G')
     ax2.plot(color[:,2], color='b', label='B')
     ax2.plot(color[:,3], color='k', label='C')
-    ax2.set(title='Color sensor', ylim=[0, 1024])
+    ax2.legend()
+    ax2.set(title='Color sensor ({})'.format(cname), ylim=[0, 1024])
+
+    p = ColorSensor.project(color)
+    ax3.plot(p[:,0], label='0')
+    ax3.plot(p[:,1], label='1')
+    ax3.legend()
+
+    # ax1, ax2 = axs2[i,:]
+
+all_colors = 
 
 plt.show()
